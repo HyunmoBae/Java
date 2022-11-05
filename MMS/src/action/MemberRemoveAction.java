37724ee6979a372ba4da7e753e2773c00ce275d2
HyunmoBae <tsi0520@cu.ac.kr>
2022-11-05 09:49:55 +0900

    1105

---
+++ MMS/src/action/MemberRemoveAction.java
@@ -0,0 +1,13 @@
+package action;
+
+import java.util.Scanner;
+
+public class MemberRemoveAction implements Action {
+
+	@Override
+	public void execute(Scanner scanner) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MMS/src/util/ConsoleUtil.java
@@ -0,0 +1,55 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import vo.MemberVO;
+
+public class ConsoleUtil {
+
+	public static MemberVO getNewMemberVO(Scanner scanner) {
+		// TODO Auto-generated method stub
+		vo.MemberVO newMemberVO = new MemberVO();
+		System.out.println("회원정보입력");
+		System.out.println("회원아이디 : ");
+		String memberId = scanner.next();
+		System.out.println("회원이름 : ");
+		String memberName = scanner.next();
+		System.out.println("회원나이 : ");
+		int memberAge = scanner.nextInt();
+		System.out.println("회원주소 : ");
+		String memberAddr = scanner.next();
+		System.out.println("회원이메일 : ");
+		String memberEmail = scanner.next(); 
+		System.out.println("회원성별 : ");
+		String memberGender = scanner.next(); 
+		
+		newMemberVO.setMemberAddr(memberAddr);
+		newMemberVO.setMemberAge(memberAge);
+		newMemberVO.setMemberEmail(memberEmail);
+		newMemberVO.setMemberGender(memberGender);
+		newMemberVO.setMemberId(memberId);
+		newMemberVO.setMemberName(memberName);
+		
+		return newMemberVO;
+	}
+
+	public void printRegistSuccessMessage(String memberName) {
+		System.out.println(memberName + " 회원등록 성공");
+	}
+	public void printRegistFailMessage(String memberName) {
+		System.out.println(memberName + " 회원등록 실패");
+	}
+
+	public void printMemberList(ArrayList<MemberVO> memberList) {
+		if(memberList.size() == 0) {
+			System.out.println("등록된 회원이 없습니다.");
+		}
+		else {
+			for (int i = 0; i < memberList.size(); i++) {
+				System.out.println(memberList.get(i));
+			}
+		}
+	}
+
+}
